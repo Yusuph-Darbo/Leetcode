5c9745f84c0937320192ff9ea58caea9724ff34f
Yusuph Darbo <yusuphdarbo@gmail.com>
2026-02-18 10:50:10 +0000

    added remove all adjacent duplicates in string easy

---
+++ Problems & solutions/Easy/remAllAdjacentDupsInString.py
@@ -0,0 +1,37 @@
+# Approach:
+# Use a stack to process the characters in the string.
+#
+# Iterate through each character in the string:
+# - If the stack is not empty and the top of the stack is equal to the current character,
+#   pop the top element from the stack to remove the duplicate pair.
+#
+# - Otherwise, push the current character onto the stack.
+#
+# This ensures that whenever two adjacent duplicate characters are found,
+# they are removed immediately.
+#
+# After processing all characters, the stack contains the final string with no adjacent duplicates.
+#
+# Join the stack into a string and return the result.
+#
+# Time Complexity: O(n) — 
+# Space Complexity: O(n) — 
+
+
+class Solution(object):
+    def removeDuplicates(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        stack = []
+
+
+        for char in s:
+            # Returns the top value of the stack
+            if stack and stack[-1] == char:
+                stack.pop()
+            else:
+                stack.append(char)
+
+        return ''.join(stack)
