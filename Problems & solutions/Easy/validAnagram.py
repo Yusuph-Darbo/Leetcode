3f076999ea59d9c44b0951a71e2e36136525a267
Yusuph Darbo <yusuphdarbo@gmail.com>
2026-03-23 15:47:09 +0000

    solved valid anagram easy

---
+++ Problems & solutions/Easy/validAnagram.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        # Sort it and compare
+        # Count the occurrences of each char and compare the values
