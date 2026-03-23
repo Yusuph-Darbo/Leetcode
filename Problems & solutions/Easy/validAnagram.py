f173298772ab460cd4083d595b1ab11f13ff8367
Yusuph Darbo <yusuphdarbo@gmail.com>
2026-03-23 15:47:38 +0000

    solved valid anagram easy

--- Problems & solutions/Easy/validAnagram.py
+++ Problems & solutions/Easy/validAnagram.py
@@ -5,5 +5,15 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        # Sort it and compare
-        # Count the occurrences of each char and compare the values
+
+        if len(s) != len(t):
+            return False
+
+        s_counter = [0] * 26
+        t_counter = [0] * 26
+
+        for i in range(len(s)):
+            s_counter[ord(s[i]) - ord("a")] += 1
+            t_counter[ord(t[i]) - ord("a")] += 1
+
+        return s_counter == t_counter
