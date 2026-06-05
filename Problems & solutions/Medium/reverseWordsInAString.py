023dea742b6476eecdcbd7cd8abdc28305ea6037
Yusuph Darbo <yusuphdarbo@gmail.com>
2026-06-05 16:26:09 +0100

    solved reverse words in a string

---
+++ Problems & solutions/Medium/reverseWordsInAString.py
@@ -0,0 +1,19 @@
+# Approach:
+# Split the string into words, then reverse their order
+# using two pointers. Join the reversed words with single
+# spaces to construct the final string.
+#
+# Time: O(n)
+# Space: O(n)
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        words = s.split()
+        l, r = 0, len(words) - 1
+
+        while l <= r:
+            words[l], words[r] = words[r], words[l]
+
+            l += 1
+            r -= 1
+
+        return " ".join(words)
