1997422647c561f7b3613c826ddb48a8da8a1c44
Yusuph Darbo <yusuphdarbo@gmail.com>
2026-08-25 16:25:10 +0100

    solved reverse string

---
+++ Problems & solutions/Easy/reverseString.py
@@ -0,0 +1,19 @@
+# Approach:
+# Use two pointers from both ends of the string and swap the characters
+# until the pointers meet.
+#
+# Time: O(n)
+# Space: O(1)
+
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        l, r = 0, len(s) - 1
+
+        while l < r:
+            s[l], s[r] = s[r], s[l]
+            l += 1
+            r -= 1
