f93864b5ba7fececf01e939a1396bde6ca383e13
Yusuph Darbo <yusuphdarbo@gmail.com>
2026-08-25 16:48:28 +0100

    solved to lower case

---
+++ Problems & solutions/Easy/toLowerCase.py
@@ -0,0 +1,12 @@
+# Approach:
+# Use Python's built-in lower() method to convert every uppercase character
+# in the string to lowercase.
+#
+# Time: O(n)
+# Space: O(n)
+
+
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+
+        return s.lower()
